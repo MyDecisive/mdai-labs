810198a22e47b8c051f42d549f3298f893fe18a6
o11yguru <jill@mydecisive.ai>
2026-01-20 10:17:00 -0700

    fix: all pii fields working

--- 0.9.0/use_cases/pii/scripts/emit-vars.cjs
+++ 0.9.0/use_cases/pii/scripts/emit-vars.cjs
@@ -26,7 +26,10 @@ for (const [key, obj] of Object.entries(vars)) {
 
   // Always emit the base variable
   process.stdout.write(
-    JSON.stringify({ name: key, payload: { data: toPipeIfArray(data) } }) + "\n"
+    JSON.stringify({
+      name: key,
+      payload: { data, }
+    }) + "\n"
   );
 
   // If it's a regex and flagged ottl:true, also emit KEY_OTTL
